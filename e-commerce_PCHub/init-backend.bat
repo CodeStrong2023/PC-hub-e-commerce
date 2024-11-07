c3845db5ce68579715018dcfd46bd6025b6c6b76
Leandro Eugenio <leoeugenio16@gmail.com>
2024-11-06 22:18:36 -0300

    actualizacion de init backend

--- e-commerce_PCHub/init-backend.bat
+++ e-commerce_PCHub/init-backend.bat
@@ -9,7 +9,34 @@ set /p PGPASSWORD=Ingrese la contraseña del usuario %PGUSER% (por defecto: admi
 :: Establecer valores por defecto si no se ingresan
 if "%PGUSER%"=="" set PGUSER=root
 if "%PGPASSWORD%"=="" set PGPASSWORD=admin
+:: Configurar archivo .env
+(
+echo HOST=0.0.0.0
+echo PORT=1337
 
+echo.
+echo # Database
+echo DATABASE_CLIENT=postgres
+echo DATABASE_HOST=localhost
+echo DATABASE_PORT=5432
+echo DATABASE_NAME=ecommerce-PCHUB
+echo DATABASE_USERNAME=%PGUSER%
+echo DATABASE_PASSWORD=%PGPASSWORD%
+echo DATABASE_SSL=false
+
+echo.
+echo # Secrets
+echo APP_KEYS=hyhgd5mM9RJXkHqAP5BSyg==,9A0E3hJOomwh3Ym6EKdxPw==,MiKYf5CFcJ0beLyW8o3V1A==,7hO5vmhyiubfMi/hALAmgw==
+echo API_TOKEN_SALT=WM6Rr+PL7vXp+D4YU/26CA==
+echo ADMIN_JWT_SECRET=R85ZNQ8F1q7I/5UFn8J/3A==
+echo TRANSFER_TOKEN_SALT=ozAPDtQeVOH8GJtTehfiHw==
+
+echo.
+echo # Storage
+echo DATABASE_FILENAME=.tmp/data.db
+) > .env
+
+echo Archivo .env configurado correctamente.
 :: Crear base de datos, ignorando errores si ya existe
 psql -U %PGUSER% -d postgres -c "CREATE DATABASE \"ecommerce-PCHUB\"" > nul 2>&1
 if %errorlevel% equ 0 (
@@ -47,34 +74,7 @@ if %errorlevel% equ 0 (
     exit /b 1
 )
 
-:: Configurar archivo .env
-(
-echo HOST=0.0.0.0
-echo PORT=1337
 
-echo.
-echo # Database
-echo DATABASE_CLIENT=postgres
-echo DATABASE_HOST=localhost
-echo DATABASE_PORT=5432
-echo DATABASE_NAME=ecommerce-PCHUB
-echo DATABASE_USERNAME=%PGUSER%
-echo DATABASE_PASSWORD=%PGPASSWORD%
-echo DATABASE_SSL=false
-
-echo.
-echo # Secrets
-echo APP_KEYS=hyhgd5mM9RJXkHqAP5BSyg==,9A0E3hJOomwh3Ym6EKdxPw==,MiKYf5CFcJ0beLyW8o3V1A==,7hO5vmhyiubfMi/hALAmgw==
-echo API_TOKEN_SALT=WM6Rr+PL7vXp+D4YU/26CA==
-echo ADMIN_JWT_SECRET=R85ZNQ8F1q7I/5UFn8J/3A==
-echo TRANSFER_TOKEN_SALT=ozAPDtQeVOH8GJtTehfiHw==
-
-echo.
-echo # Storage
-echo DATABASE_FILENAME=.tmp/data.db
-) > .env
-
-echo Archivo .env configurado correctamente.
 
 
 :: Instalar dependencias de Strapi
